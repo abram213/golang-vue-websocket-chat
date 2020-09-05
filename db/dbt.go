c60bd08a671a571ce4adf889584e1b1166405b29
abram213 <abram13504@yandex.ru>
2020-09-05 12:18:26 +0300

    add funcs to DatabaseTest struct to implement DataLayer interface

--- db/dbt.go
+++ db/dbt.go
@@ -26,11 +26,25 @@ func (db *DatabaseTest) CreateUser(user *model.User) error {
 }
 
 func (db *DatabaseTest) UserExistByUsername(username string) bool {
-	return true
+	if db.Users != nil {
+		for _, user := range db.Users {
+			if user.Username == username {
+				return true
+			}
+		}
+	}
+	return false
 }
 
-func (db *DatabaseTest) UserExistByID(username string) bool {
-	return true
+func (db *DatabaseTest) UserExistByID(id uint) bool {
+	if db.Users != nil {
+		for _, user := range db.Users {
+			if user.ID == id {
+				return true
+			}
+		}
+	}
+	return false
 }
 
 func (db *DatabaseTest) AddUserFriend(userID, friendID uint) error {
@@ -42,7 +56,18 @@ func (db *DatabaseTest) DeleteUserFriend(userID, friendID uint) error {
 }
 
 func (db *DatabaseTest) FriendshipExist(userID, friendID uint) bool {
-	return true
+	if db.Users != nil {
+		for _, user := range db.Users {
+			if user.ID == userID && user.Friends != nil {
+				for _, friend := range user.Friends {
+					if friend.ID == friendID {
+						return true
+					}
+				}
+			}
+		}
+	}
+	return false
 }
 
 func (db *DatabaseTest) GetUsers() (*[]model.User, error) {
@@ -57,11 +82,47 @@ func (db *DatabaseTest) GetUserFriends(id uint) (*[]model.User, error) {
 	return &db.Users, nil
 }
 
-//Util methods
-func (db *DatabaseTest) CloseDB() error {
+//Chat methods
+func (db *DatabaseTest) CreateChat(chat *model.Chat) (string, error) {
+	return chat.Identifier, nil
+}
+
+func (db *DatabaseTest) GetChatByID(id uint) (*model.Chat, error) {
+	chat := &model.Chat{
+		Model: model.Model{ID: id},
+	}
+	return chat, nil
+}
+
+func (db *DatabaseTest) GetChatByIdentifier(i string) (*model.Chat, error) {
+	chat := &model.Chat{
+		Identifier: i,
+	}
+	return chat, nil
+}
+func (db *DatabaseTest) GetUserChats(id uint) (*[]model.Chat, error) {
+	chats := []model.Chat{
+		{UserID: id},
+		{UserID: id},
+		{UserID: id},
+	}
+	return &chats, nil
+}
+
+func (db *DatabaseTest) DeleteChat(id uint) error {
 	return nil
 }
 
+//Message methods
+func (db *DatabaseTest) CreateMessage(m *model.Message) error {
+	return nil
+}
+
+//Util methods
 func (db *DatabaseTest) Migrate(values ...interface{}) {}
 
 func (db *DatabaseTest) DropTables(values ...interface{}) {}
+
+func (db *DatabaseTest) CloseDB() error {
+	return nil
+}
